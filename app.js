7f862e8dc72a6bfa0bf04d77937fc8f5c417598b
paul <paul@paul-eos.(none)>
2014-02-06 03:42:38 +0000

    improved error logging

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ smtp.on("dataReady", function(connection, callback){
 
 var sendToOatmail = function(mail_object) {
     var reqOptions = {
-        url: 'https://oatmail.io/api/recieve',
+        url: 'http://dragonstone-nodejs-56183.euw1.nitrousbox.com:3000/api/recieve',
         method: "POST",     
         body: mail_object,
         json: true,
@@ -72,8 +72,11 @@ var sendToOatmail = function(mail_object) {
         if(!error && response.statusCode == 200) {
            deleteTmpEmail(mail_object.headers.oatmailid);
            console.log("Sent email to oatmail app, success! Code: " + response.statusCode + ". Deleting the email from /tmp/");            
-        } else {
-           console.log("Error sending email to oatmail app, need some kind of logic to keep trying? " + response);
+        } else if(response.statusCode == 404) {
+           console.log("error: no oatmail address found in the email. " + JSON.stringify(mail_object.to) + JSON.stringify(mail_object.cc) + JSON.stringify(mail_object.bcc));
+        }
+        else {
+            console.log("error sending mail_object to oatmail/tent " + response.statusCode);
         }
     });
 }
